f761a70819aeef668f4cce0b7e2a3f2a55ba7b1c
Michał Kępień <yafut@kempniu.pl>
2023-03-31 12:03:31 +0200

    Add special handling for GCC Static Analyzer
    GCC Static Analyzer reports some false positives about uninitialized
    variables because it does not understand that util_get_errno() is
    guaranteed to return a negative value at runtime (or else the program
    will abort()).  Unfortunately, adding -fanalyzer to CFLAGS does not
    cause any special preprocessor macro to be defined by GCC (like Clang
    does with its __clang_analyzer__ macro).
    
    To work around these issues, define the __gcc_analyzer__ preprocessor
    macro when -fanalyzer is in use (to mimic what happens when Clang Static
    Analyzer is in use) and handle GCC Static Analyzer identically as Clang
    Static Analyzer to prevent the former from reporting false positives
    related to util_get_errno().

--- src/util.h
+++ src/util.h
@@ -4,27 +4,27 @@
 
 #pragma once
 
-#ifdef __clang_analyzer__
+#if defined(__clang_analyzer__) || defined(__gcc_analyzer__)
 
 /*
- * Make util_get_errno() always return -1 when Clang Static Analyzer is in use.
- * This silences its warnings triggered by 'errno' possibly being zero on
- * errors, which should never be the case.  However...
+ * Make util_get_errno() always return -1 when Clang Static Analyzer or GCC
+ * Static Analyzer is in use.  This silences its warnings triggered by 'errno'
+ * possibly being zero on errors, which should never be the case.  However...
  */
 #define util_get_errno() -1
 
-#else /* __clang_analyzer__ */
+#else /* defined(__clang_analyzer__) || defined(__gcc_analyzer__) */
 
 /*
- * ...rather than assume things, when Clang Static Analyzer is _not_ in use,
- * instead of accessing 'errno' directly, use a helper function that actually
- * checks whether 'errno' lives up to its promise at run time (and call abort()
- * if it does not).
+ * ...rather than assume things, when Clang Static Analyzer or GCC Static
+ * Analyzer is _not_ in use, instead of accessing 'errno' directly, use a
+ * helper function that actually checks whether 'errno' lives up to its promise
+ * at run time (and call abort() if it does not).
  */
 #define util_get_errno() util_get_errno_location(__FILE__, __LINE__, __func__)
 int util_get_errno_location(const char *file, int line, const char *func);
 
-#endif /* __clang_analyzer__ */
+#endif /* defined(__clang_analyzer__) || defined(__gcc_analyzer__) */
 
 char *util_get_error(int err);
 int util_parse_number(const char *string, int base, unsigned int *result);
