ca92271e948101d354ca6589424f766d882ddec4
Michał Kępień <yafut@kempniu.pl>
2024-05-13 12:52:22 +0200

    Support disabling Yaffs2 checkpoints
    By default, Yaffs2 code stores a snapshot of its runtime state (called a
    "checkpoint") in the file system when the latter gets unmounted or
    sync()'d.  Reading such a checkpoint speeds up file system mounting.
    
    Checkpoints are merely a performance optimization and maintaining them
    requires extra storage space.  Add a new command-line option, -P, which
    prevents checkpoints from being read or written, saving some storage
    space at the cost of slower file system mounting.  Update README.md
    accordingly.

--- src/options.h
+++ src/options.h
@@ -17,6 +17,7 @@
 	"[ -B <bytes> ] "                                                      \
 	"[ -T ] "                                                              \
 	"[ -E ] "                                                              \
+	"[ -P ] "                                                              \
 	"[ -v ] "                                                              \
 	"[ -h ] "                                                              \
 	"\n\n"                                                                 \
@@ -31,6 +32,7 @@
 	"    -B  force Yaffs block size to <bytes> (use 'k' suffix for KiB)\n" \
 	"    -T  force inband tags\n"                                          \
 	"    -E  disable ECC for tags\n"                                       \
+	"    -P  disable Yaffs2 checkpoints\n"                                 \
 	"    -v  verbose output (can be used up to two times)\n"               \
 	"    -h  show usage information and exit\n"
 
@@ -53,6 +55,7 @@ struct opts {
 	int block_size;
 	bool force_inband_tags;
 	bool disable_ecc_for_tags;
+	bool disable_checkpoints;
 };
 
 void options_parse_env(void);
